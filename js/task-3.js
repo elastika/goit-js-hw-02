6361003422a71fef21d8de928d38145dfecabc01
elastika <anastation13@GMAIL.COM>
2023-12-05 01:49:08 +0200

    tasks completed

--- js/task-3.js
+++ js/task-3.js
@@ -1,12 +1,18 @@
-function getElementWidth(content, padding, border) {
-    const a = Number.parseFloat(content);
-    const b = Number.parseFloat(padding);
-    const c = Number.parseFloat(border);
+function checkForSpam(message) {
+    let lowermessage=message.toLowerCase();
+    const hasSpam=lowermessage.includes("spam","sale");
+    const hasSale=lowermessage.includes("sale");
     
-    return a + b * 2 + c * 2
-};
+    if (hasSpam) return `true`;
+    else if (hasSale) return `true`;
+    else return `false`;
 
+}
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+console.log(checkForSpam("Latest technology news")); // false
+console.log(checkForSpam("JavaScript weekly newsletter")); // false
+console.log(checkForSpam("Get best sale offers now!")); // true
+console.log(checkForSpam("Amazing SalE, only tonight!")); // true
+console.log(checkForSpam("Trust me, this is not a spam message")); // true
+console.log(checkForSpam("Get rid of sPaM emails. Our book in on sale!")); // true
+console.log(checkForSpam("[SPAM] How to earn fast money?")); // true
